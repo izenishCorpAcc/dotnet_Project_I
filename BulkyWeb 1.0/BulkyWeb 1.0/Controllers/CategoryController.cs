6e09104fef1184e46f7cf5f81ee0a242069afc4c
Prajapati <jenish.prajapati@cotiviti.com>
2023-05-08 11:15:56 +0545

    Passing id to the server

--- BulkyWeb 1.0/BulkyWeb 1.0/Controllers/CategoryController.cs
+++ BulkyWeb 1.0/BulkyWeb 1.0/Controllers/CategoryController.cs
@@ -17,6 +17,8 @@ namespace BulkyWeb_1._0.Controllers
             List<Category> objCategoryList=_db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        //IF HTTPPOST isn't defined then by default its a GET method
         public IActionResult Create()
         {
             return View();
@@ -38,5 +40,38 @@ namespace BulkyWeb_1._0.Controllers
             return View();
           
         }
+        //For EDIT
+        public IActionResult Edit(int? id)
+        {
+            if(id == null||id==0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.FirstOrDefault(c => c.Id == id);
+           
+            if(categoryFromDB != null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The fields can't be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
     }
 }
